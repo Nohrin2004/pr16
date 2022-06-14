809dcfa309d406b4212bfcd59bd9c3532a5fc4f2
Nohrin2004 <107471725+Nohrin2004@users.noreply.github.com>
2022-06-14 15:15:19 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-namespace pr16_3
+using System.IO;
+using System.Collections.Generic; 
+namespace pr16
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            double[] a = new double [10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.WriteLine(a[i] + " ");
-            }
-            List<double> b = new List<double>();
-            List<int> c = new List<int>();
-            for (int i = 0; i < a.Length; i++)
-            {
-                int c1 = 1;
-                c.Clear();
-                c.Add(1);
-                for (int j = 0; j < a.Length; j++)
-                {
+            Console.WriteLine(" ");
+            StreamReader file = new StreamReader("1.txt");
+            string a = file.ReadToEnd().ToLower();
+            file.Close();
+            string line = Console.ReadLine().ToLower();
+            string[] b = a.Split();
+            int kol = 0;
+            List<int> list = new List<int>();
+            if (b.Contains<string>(line))
+                for (int i = 0; i < a.Length; i++)
+                    list.Add(1);
+            Console.WriteLine($"Найдено {list.Sum()} вхождениий из поискового запроса {line}");
 
 
-                    if (i != j)
-                    {
-                        if (a[i] == a[j])
-                        {
-                            c1++;
-                            c.Add(1);
-                        }
-                    }
 
-                }
-                b.Add(c.Sum());
-
-            }
-            Console.WriteLine("!");
-
-            Console.WriteLine(b[7]);
-
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.Write($"{i + 1}) {a[i]} == {a[i]}");
-                Console.WriteLine();
-            }
-            double[] fin = new double[a.Length];
-            Console.WriteLine("Финальный массив");
-            Console.WriteLine();
-            for (int i = 0; i < fin.Length; i++)
-            {
-                fin[i] = a[i] * b[i];
-                Console.Write(fin[i] + " ");
-
-            }
         }
     }
 }
